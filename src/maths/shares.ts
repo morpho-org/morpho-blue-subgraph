41a537245967f3f80e5c604997a6b7f682d6101c
julien <julien@morpho.xyz>
2023-08-18 11:03:12 +0200

    feat: add maths

---
+++ src/maths/shares.ts
@@ -0,0 +1,15 @@
+import {BIGINT_WAD} from "../sdk/constants";
+import {BigInt} from "@graphprotocol/graph-ts";
+import {mulDivDown, mulDivUp} from "./maths";
+
+
+const VIRTUAL_SHARES = BIGINT_WAD;
+const VIRTUAL_ASSETS = BigInt.zero();
+
+export function toAssetsUp(shares: BigInt, totalShares: BigInt, totalAssets: BigInt): BigInt {
+    return mulDivUp(shares, totalAssets.plus(VIRTUAL_ASSETS), totalShares.plus(VIRTUAL_SHARES));
+}
+
+export function toAssetsDown(shares: BigInt, totalShares: BigInt, totalAssets: BigInt): BigInt {
+    return mulDivDown(shares, totalAssets.plus(VIRTUAL_ASSETS), totalShares.plus(VIRTUAL_SHARES));
+}
