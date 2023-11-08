e611aeda4fd40da2eb302455d0fd8cc052a99e39
julien <julien@morpho.xyz>
2023-11-08 16:16:46 +0100

    fix(metaMorpho): remove default address in metamorpho config

--- src/meta-morpho-factory.ts
+++ src/meta-morpho-factory.ts
@@ -1,8 +1,11 @@
-import { Address, BigInt } from "@graphprotocol/graph-ts";
+import { BigInt } from "@graphprotocol/graph-ts";
 
 import { CreateMetaMorpho as CreateMetaMorphoEvent } from "../generated/MetaMorphoFactory/MetaMorphoFactory";
 import { MetaMorpho } from "../generated/schema";
 import { MetaMorpho as MetaMorphoTemplate } from "../generated/templates";
+
+import { AccountManager } from "./sdk/account";
+import { TokenManager } from "./sdk/token";
 
 export function handleCreateMetaMorpho(event: CreateMetaMorphoEvent): void {
   MetaMorphoTemplate.create(event.params.metaMorpho);
@@ -11,17 +14,17 @@ export function handleCreateMetaMorpho(event: CreateMetaMorphoEvent): void {
   metaMorpho.name = event.params.name;
   metaMorpho.symbol = event.params.symbol;
   metaMorpho.decimals = 18;
-  metaMorpho.asset = event.params.asset;
+  metaMorpho.asset = new TokenManager(event.params.asset, event).getToken().id;
 
-  metaMorpho.owner = event.params.initialOwner;
+  metaMorpho.owner = new AccountManager(
+    event.params.initialOwner
+  ).getAccount().id;
   metaMorpho.curators = [];
   metaMorpho.allocators = [];
-  metaMorpho.guardian = Address.zero();
 
   metaMorpho.timelock = event.params.initialTimelock;
 
   metaMorpho.fee = BigInt.zero();
-  metaMorpho.feeRecipient = Address.zero();
   metaMorpho.feeAccrued = BigInt.zero();
   metaMorpho.feeAccruedAssets = BigInt.zero();
 
