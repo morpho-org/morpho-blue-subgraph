9148a41a46514aa441390d94ba3c5b5ccec7e9ab
julien <julien@morpho.xyz>
2024-02-13 09:02:14 +0100

    feat(prices): price new tokens

--- src/fetchUsdTokenPrice.ts
+++ src/fetchUsdTokenPrice.ts
@@ -1,4 +1,4 @@
-import { Address, BigDecimal, BigInt, log } from "@graphprotocol/graph-ts";
+import { Address, BigDecimal, BigInt } from "@graphprotocol/graph-ts";
 
 import { ChainlinkPriceFeed } from "../generated/MorphoBlue/ChainlinkPriceFeed";
 import { ERC4626 } from "../generated/MorphoBlue/ERC4626";
@@ -11,15 +11,19 @@ import { BIGDECIMAL_ONE, BIGDECIMAL_WAD, BIGINT_WAD } from "./sdk/constants";
 const wbib01 = Address.fromString(
   "0xca2a7068e551d5c4482eb34880b194e4b945712f"
 ).toHexString();
+
 const wstEth = Address.fromString(
   "0x7f39c581f595b53c5cb19bd0b3f8da6c935e2ca0"
 ).toHexString();
+
 const weth = Address.fromString(
   "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"
 ).toHexString();
+
 const wbtc = Address.fromString(
   "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599"
 ).toHexString();
+
 const usdc = Address.fromString(
   "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48"
 ).toHexString();
@@ -27,6 +31,7 @@ const usdc = Address.fromString(
 const sdai = Address.fromString(
   "0x83F20F44975D03b1b09e64809B757c47f942BEeA"
 ).toHexString();
+
 const dai = Address.fromString(
   "0x6B175474E89094C44Da98b954EedeAC495271d0F"
 ).toHexString();
@@ -34,6 +39,7 @@ const dai = Address.fromString(
 const weETH = Address.fromString(
   "0xCd5fE23C85820F7B72D0926FC9b05b43E359b7ee"
 ).toHexString();
+
 const osETH = Address.fromString(
   "0xf1C9acDc66974dFB6dEcB12aA385b9cD01190E38"
 ).toHexString();
@@ -41,12 +47,25 @@ const osETH = Address.fromString(
 const usdt = Address.fromString(
   "0xdac17f958d2ee523a2206206994597c13d831ec7"
 ).toHexString();
+
 const rEth = Address.fromString(
   "0xae78736cd615f374d3085123a210448e74fc6393"
 ).toHexString();
 
 const pyUsd = Address.fromString(
   "0x6c3ea9036406852006290770BEdFcAbA0e23A0e8"
+).toHexString();
+
+const wusdm = Address.fromString(
+  "0x57f5e098cad7a3d1eed53991d4d66c45c9af7812"
+).toHexString();
+
+const wbc3m = Address.fromString(
+  "0x95d7337d43340e2721960dc402d9b9117f0d81a2"
+).toHexString();
+
+const EURe = Address.fromString(
+  "0x3231cb76718cdef2155fc47b5286d82e6eda273f"
 ).toHexString();
 
 const usdPriceFeeds = new Map<string, string>()
@@ -91,6 +110,12 @@ const usdPriceFeeds = new Map<string, string>()
     Address.fromString(
       "0x3E7d1eAB13ad0104d2750B8863b489D65364e32D"
     ).toHexString()
+  )
+  .set(
+    EURe,
+    Address.fromString(
+      "0xb49f677943BC038e9857d61E7d053CaA2C1734C1"
+    ).toHexString()
   );
 
 const ethPriceFeeds = new Map<string, string>().set(
@@ -98,10 +123,12 @@ const ethPriceFeeds = new Map<string, string>().set(
   Address.fromString("0x66ac817f997Efd114EDFcccdce99F3268557B32C").toHexString()
 );
 
+const eurPriceFeeds = new Map<string, string>().set(
+  wbc3m,
+  Address.fromString("0x83ec02059f686e747392a22ddfed7833ba0d7ce3").toHexString()
+);
+
 export function fetchUsdTokenPrice(tokenAddress: Address): BigDecimal {
-  log.warning("fetchUsdTokenPrice({})", [tokenAddress.toHexString()]);
-  log.info("fetchUsdTokenPrice({})", [tokenAddress.toHexString()]);
-  log.error("fetchUsdTokenPrice({})", [tokenAddress.toHexString()]);
   if (usdPriceFeeds.has(tokenAddress.toHexString())) {
     const chainlinkPriceFeed = ChainlinkPriceFeed.bind(
       Address.fromString(usdPriceFeeds.get(tokenAddress.toHexString()))
@@ -117,6 +144,7 @@ export function fetchUsdTokenPrice(tokenAddress: Address): BigDecimal {
           .toBigDecimal()
       );
   }
+
   if (ethPriceFeeds.has(tokenAddress.toHexString())) {
     const chainlinkPriceFeed = ChainlinkPriceFeed.bind(
       Address.fromString(ethPriceFeeds.get(tokenAddress.toHexString()))
@@ -133,6 +161,22 @@ export function fetchUsdTokenPrice(tokenAddress: Address): BigDecimal {
       .times(fetchUsdTokenPrice(Address.fromString(weth)));
   }
 
+  if (eurPriceFeeds.has(tokenAddress.toHexString())) {
+    const chainlinkPriceFeed = ChainlinkPriceFeed.bind(
+      Address.fromString(eurPriceFeeds.get(tokenAddress.toHexString()))
+    );
+    return chainlinkPriceFeed
+      .latestRoundData()
+      .getAnswer()
+      .toBigDecimal()
+      .div(
+        BigInt.fromString("10")
+          .pow(chainlinkPriceFeed.decimals() as u8)
+          .toBigDecimal()
+      )
+      .times(fetchUsdTokenPrice(Address.fromString(EURe)));
+  }
+
   if (tokenAddress.equals(Address.fromString(wstEth))) {
     const wstEthContract = WstEth.bind(Address.fromString(wstEth));
     return wstEthContract
@@ -141,6 +185,7 @@ export function fetchUsdTokenPrice(tokenAddress: Address): BigDecimal {
       .div(BIGDECIMAL_WAD)
       .times(fetchUsdTokenPrice(Address.fromString(weth)));
   }
+
   if (tokenAddress.equals(Address.fromString(rEth))) {
     const rEthContract = REth.bind(Address.fromString(rEth));
     return rEthContract
@@ -149,6 +194,7 @@ export function fetchUsdTokenPrice(tokenAddress: Address): BigDecimal {
       .div(BIGDECIMAL_WAD)
       .times(fetchUsdTokenPrice(Address.fromString(weth)));
   }
+
   if (tokenAddress.equals(Address.fromString(sdai))) {
     const sDaiContract = ERC4626.bind(Address.fromString(sdai));
     return sDaiContract
@@ -157,6 +203,16 @@ export function fetchUsdTokenPrice(tokenAddress: Address): BigDecimal {
       .div(BIGDECIMAL_WAD)
       .times(fetchUsdTokenPrice(Address.fromString(dai)));
   }
+
+  if (tokenAddress.equals(Address.fromString(wusdm))) {
+    const erc4626 = ERC4626.bind(Address.fromString(wusdm));
+    // exchange 1 usdm = 1 usd
+    return erc4626
+      .convertToAssets(BIGINT_WAD)
+      .toBigDecimal()
+      .div(BIGDECIMAL_WAD);
+  }
+
   if (tokenAddress.equals(Address.fromString(pyUsd))) {
     // price is hardcoded at 1 since the token is regulated. This is also the case in the trusted oracles.
     return BIGDECIMAL_ONE;
