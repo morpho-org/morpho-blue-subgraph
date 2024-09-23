c59a5fbc0ce3603c80e3054b8aa41ec3355e2c4e
Julien <julien@morpho.xyz>
2024-09-23 10:15:22 +0200

    feat(public-allocator): add public allocator to the template

---
+++ src/public-allocator.ts
@@ -0,0 +1,20 @@
+import {
+  PublicReallocateTo,
+  PublicWithdrawal,
+  SetAdmin,
+  SetFee,
+  SetFlowCaps,
+  TransferFee,
+} from "../generated/PublicAllocator/PublicAllocator";
+
+export function handlePublicReallocateTo(event: PublicReallocateTo): void {}
+
+export function handlePublicWithdrawal(event: PublicWithdrawal): void {}
+
+export function handleSetAdmin(event: SetAdmin): void {}
+
+export function handleSetFee(event: SetFee): void {}
+
+export function handleSetFlowCaps(event: SetFlowCaps): void {}
+
+export function handleTransferFee(event: TransferFee): void {}
