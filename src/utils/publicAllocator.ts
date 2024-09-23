85df728495769eeb74d59ebcce8be0f27b64a6f4
Julien <julien@morpho.xyz>
2024-09-23 13:56:10 +0200

    fix(public-allocator): network without public allocator fallbacks on zeroAddress

--- src/utils/publicAllocator.ts
+++ src/utils/publicAllocator.ts
@@ -2,7 +2,6 @@ import { Address, dataSource, log } from "@graphprotocol/graph-ts";
 
 export function getPublicAllocatorAddress(): Address {
   const network = dataSource.network();
-  log.warning("Network id: {}", [network]);
   if (network == "mainnet") {
     return Address.fromString("0xfd32fA2ca22c76dD6E550706Ad913FC6CE91c75D");
   }
@@ -10,7 +9,6 @@ export function getPublicAllocatorAddress(): Address {
     return Address.fromString("0xA090dD1a701408Df1d4d0B85b716c87565f90467");
   }
 
-  log.warning("No public allocator for network: {}", [network]);
-  return Address.fromString("0xfd32fA2ca22c76dD6E550706Ad913FC6CE91c75D");
-  // TODO: throw an error instead of returning a default value
+  log.warning("Unsupported Network id: {}", [network]);
+  return Address.zero();
 }
