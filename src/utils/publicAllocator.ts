e2e481f5023a8daafaf93ca0991b9ee1651a42b8
Julien <julien@morpho.xyz>
2025-02-14 11:40:13 +0100

    feat: support arbitrum

--- src/utils/publicAllocator.ts
+++ src/utils/publicAllocator.ts
@@ -2,18 +2,16 @@ import { Address, dataSource, log } from "@graphprotocol/graph-ts";
 
 export function getPublicAllocatorAddress(): Address {
   const network = dataSource.network();
-  if (network == "mainnet") {
-    return Address.fromString("0xfd32fA2ca22c76dD6E550706Ad913FC6CE91c75D");
+  switch (network) {
+    case "mainnet":
+      return Address.fromString("0xfd32fA2ca22c76dD6E550706Ad913FC6CE91c75D");
+    case "base":
+      return Address.fromString("0xA090dD1a701408Df1d4d0B85b716c87565f90467");
+    case "optimism":
+      return Address.fromString("0x0d68a97324E602E02799CD83B42D337207B40658");
+    case "arbitrum-one":
+      return Address.fromString("0x769583Af5e9D03589F159EbEC31Cc2c23E8C355E");
+    default:
+      return Address.zero();
   }
-
-  if (network == "base") {
-    return Address.fromString("0xA090dD1a701408Df1d4d0B85b716c87565f90467");
-  }
-
-  if (network == "optimism") {
-    return Address.fromString("0x0d68a97324E602E02799CD83B42D337207B40658");
-  }
-
-  log.warning("Unsupported Network id: {}", [network]);
-  return Address.zero();
 }
